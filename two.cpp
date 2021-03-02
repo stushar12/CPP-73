3c34eaa9af377d0c6fd29c0c330126e775c65b04
Tushar Saxena <stushar345@gmail.com>
2021-03-02 17:09:49 +0530

    Add files via upload

--- two.cpp
+++ two.cpp
@@ -1,25 +1,72 @@
-int isroot(Node *root)
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
 {
-    if(root==NULL)
-    return 0;
-    int lh=isroot(root->left);
-    if(lh<0)
-    return -1;
-    int rh=isroot(root->right);
-    if(rh<0)
-    return -1;
-    if(abs(lh-rh)>1)
-    {
-        return -1;
-    }
-    else
-    return max(lh,rh)+1;
+	int data; 
+	Node* left;
+	Node* right;
+};
+
+Node* GetNewNode(int data) 
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
 }
-bool isBalanced(Node *root)
+
+Node* Insert(Node* root,int data) 
 {
-int a=isroot(root);
-if(a==-1)
-return false;
-else
-return true;
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted is lesser, insert in left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert in right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+int balanced_BST(Node* root)
+{
+	if(root==NULL)
+	return 0;
+
+	int lh=balanced_BST(root->left);
+	if(lh==-1)
+	return -1;
+
+	int rh=balanced_BST(root->right);
+	if(rh==-1)
+	return -1;
+
+	if(abs(lh-rh)>1)
+	return -1;
+	else
+	return max(lh,rh)+1;
+
+}
+
+
+int main() 
+{
+	Node* root = NULL;  // Creating an empty tree
+
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,8);
+	root = Insert(root,12);
+	root = Insert(root,25);
+
+	cout<<balanced_BST(root);
+
 }
